4956e526700393604c962079cdae7b2d3c6ab5ce
gorillaglass1 <155641361+gorillaglass1@users.noreply.github.com>
2024-06-19 12:38:46 +0900

    Add files via upload

---
+++ chap_07/_01_Class.java
@@ -0,0 +1,19 @@
+package chap_07;
+
+public class _01_Class {
+    public static void main(String[] args) {
+        String modelName = "까망이";
+        String resolution = "FHD";
+        int price = 200000;
+        String color = "블랙";
+
+        String modelName2 = "하양이";
+        String resolution2 = "UHD";
+        int price2 = 300000;
+        String color2 = "화이트";
+
+        BlackBox bbox = new BlackBox();
+
+        BlackBox bbox2 = new BlackBox();
+    }
+}
