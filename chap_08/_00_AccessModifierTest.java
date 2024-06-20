8a889d12c0143235e5255232f1c13449ec9e665c
gorillaglass1 <155641361+gorillaglass1@users.noreply.github.com>
2024-06-20 16:10:31 +0900

    Add files via upload

---
+++ chap_08/_00_AccessModifierTest.java
@@ -0,0 +1,15 @@
+package chap_08;
+
+import chap_07.BlackBoxRefurbish;
+//import chap_07.*;
+
+public class _00_AccessModifierTest {
+    public static void main(String[] args) {
+        BlackBoxRefurbish b1 = new BlackBoxRefurbish();
+        b1.modelName = "까망이";
+//        b1.resolution = "FHD";
+        //b1.price = 2000000;
+        //b1.color = "블랙";
+
+    }
+}
