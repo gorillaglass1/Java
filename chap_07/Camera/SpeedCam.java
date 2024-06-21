87e121cd9344b1ece7a44898e20c44b1dff0be36
gorillaglass1 <155641361+gorillaglass1@users.noreply.github.com>
2024-06-21 17:03:04 +0900

    Add files via upload

---
+++ chap_07/Camera/SpeedCam.java
@@ -0,0 +1,27 @@
+package chap_07.Camera;
+
+public class SpeedCam extends Camera{
+    public SpeedCam() {
+        //this.name = "과속단속 카메라";
+        super("과속 단속 카메라");
+    }
+
+    public void takePicture() {
+        super.takePicture();
+        checkSpeed();
+        recognizeLicensePlate();
+    }
+
+    public void checkSpeed() {
+        System.out.println("속도를 측정합니다.");
+    }
+
+    public void recognizeLicensePlate() {
+        System.out.println("차량 번호를 인식합니다.");
+    }
+    
+    @Override
+    public void showMainFeature() {
+        System.out.println(this.name + "의 주요 기능 : 속도 측정, 번호 인식");
+    }
+}
