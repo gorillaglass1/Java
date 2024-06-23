3944f922ab06c3dd690dcc99cb0fc1cba8fae612
gorillaglass1 <155641361+gorillaglass1@users.noreply.github.com>
2024-06-23 17:40:15 +0900

    Add files via upload

---
+++ chap_08/_01_AbstractClass.java
@@ -0,0 +1,17 @@
+package chap_08;
+
+import chap_08.camera.Camera;
+import chap_08.camera.FactoryCam;
+import chap_08.camera.SpeedCam;
+
+public class _01_AbstractClass {
+    public static void main(String[] args) {
+        //Camera camera = new Camera();
+
+        FactoryCam factoryCam = new FactoryCam();
+        factoryCam.showMainFeature();
+
+        SpeedCam speedCam = new SpeedCam();
+        speedCam.showMainFeature();
+    }
+}
